213f72ae86fc362b7e839af2048acc8805abec06
Ian Morgan <ian.j.morgan@gmail.com>
2022-01-01 21:22:03 +0000

    fixed compiler warnings

--- impl/src/main/kotlin/mycorda/app/sks/SimpleKVStore.kt
+++ impl/src/main/kotlin/mycorda/app/sks/SimpleKVStore.kt
@@ -3,7 +3,6 @@ package mycorda.app.sks
 import mycorda.app.rss.JsonSerialiser
 import mycorda.app.types.UniqueId
 
-
 class SimpleKVStore(initialCapacity: Int = 10) : SKS {
     private val lookup = HashMap<UniqueId, SKSValue>(initialCapacity)
     private val rss = JsonSerialiser()
@@ -15,6 +14,7 @@ class SimpleKVStore(initialCapacity: Int = 10) : SKS {
     override fun <T> getDeserialised(key: UniqueId): T {
         val raw = get(key)
         if (raw.type == SKSValueType.Serialisable) {
+            @Suppress("UNCHECKED_CAST")
             return rss.deserialiseData(raw.value as String).any() as T
         } else {
             throw SKSReadException("${raw.type} is not of type SKSValueType.Serialisable")
@@ -25,22 +25,22 @@ class SimpleKVStore(initialCapacity: Int = 10) : SKS {
         when (type) {
             SKSValueType.Text -> {
                 if ((value is String)) {
-                    val value = SKSValue(value, type)
-                    lookup[key] = value
+                    val sksValue = SKSValue(value, type)
+                    lookup[key] = sksValue
                 } else
                     throw SKSWriteException("value is not a valid SKSValueType.Text type")
             }
             SKSValueType.Binary -> {
                 if ((value is ByteArray)) {
-                    val value = SKSValue(value, type)
-                    lookup[key] = value
+                    val sksValue = SKSValue(value, type)
+                    lookup[key] = sksValue
                 } else
                     throw SKSWriteException("value is not a valid SKSValueType.Binary type")
             }
             SKSValueType.Serialisable -> {
                 val serialised = rss.serialiseData(value)
-                val value = SKSValue(serialised, type)
-                lookup[key] = value
+                val sksValue = SKSValue(serialised, type)
+                lookup[key] = sksValue
             }
         }
 
